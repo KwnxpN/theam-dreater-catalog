a5c813460758a622f886a3d4da34d1653fcc772a
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2026-03-26 14:46:41 +0700

    feat: connect api

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,14 +1,32 @@
 import { useParams } from "react-router-dom";
-import { mockProducts } from "@/mock/products";
+import { useEffect, useState } from "react";
 import ProductDetail from "@/components/product/ProductDetail";
+import { getProductById } from "@/api/product.api";
+import type { Product } from "@/types/product.type";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
 
-  const product = mockProducts.find(
-    (p) => p.id === Number(id)
-  );
+  useEffect(() => {
+    const fetchProduct = async () => {
+      if (!id) return;
 
+      try {
+        const data = await getProductById(Number(id));
+        setProduct(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>;
   if (!product) return <div>Product not found</div>;
 
   return <ProductDetail product={product} />;

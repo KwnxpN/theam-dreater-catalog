4d965228ea572b7126915b9c4a00a5c7b356b79c
ExxiDauS <kpinthawornrak@gmail.com>
2026-03-26 17:17:08 +0700

    feat: Added carousel to home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
 import Hero from '../components/home/Hero'
+import ProductCarousel from '../components/home/ProductCarousel'
+import { useProducts } from '@/hooks/queries/useProducts'
+import { useMemo } from 'react'
+import type { GetProductParams } from '@/types/product.type'
 
 const Home = () => {
+  const productParams = useMemo<GetProductParams>(() => ({
+    page: 1,
+    limit: 8,
+    sort: 'random',
+  }), [])
+
+  const { data: products, isLoading: isProductsLoading, isError: isProductsError, refetch: refetchProducts } = useProducts(productParams)
   return (
     <>
-      <Hero />
-      <div className="h-screen"></div>
+      <div className="flex flex-col gap-y-22">
+        <Hero />
+        <div className='min-h-[10vh]'></div>
+        <ProductCarousel products={products?.products || []} />
+      </div>
     </>
   )
 }

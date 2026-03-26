e6b58eddc304db95b03a32c2fe854799f5fa9a0c
KwnxpN <tonnum.kawin@gmail.com>
2026-03-26 18:00:01 +0700

    feat: add developers team to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,32 @@
-import { Package } from "lucide-react";
-import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Package, User } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import KwnxpN from "@/assets/images/KwnxpN.jpg";
+import ExxiDaus from "@/assets/images/ExxiDaus.png";
+import SRYT4E from "@/assets/images/SRYT4E.jpg";
+
+export type Developer = {
+  name: string;
+  avatar: string;
+  github?: string;
+};
+
+const developers: Developer[] = [
+  {
+    name: "KwnxpN",
+    avatar: KwnxpN,
+    github: "https://github.com/KwnxpN",
+  },
+  {
+    name: "ExxiDaus",
+    avatar: ExxiDaus,
+    github: "https://github.com/ExxiDauS",
+  },
+  {
+    name: "SRYT4E",
+    avatar: SRYT4E,
+    github: "https://github.com/Danny2Forever",
+  },
+];
 
 const Footer = () => {
   return (
@@ -22,6 +49,35 @@ const Footer = () => {
             We believe in quality without compromise. Every product in our catalog
             is selected with care to ensure reliability, consistency, and lasting value.
           </p>
+        </div>
+
+        {/* Grid 2: Made by */}
+        <div className="space-y-4">
+          <h3 className="font-semibold text-lg">Made with ❤️ by</h3>
+          <div className="flex flex-col justify-center gap-4">
+
+            {developers.map((dev) => (
+              <div className="flex items-center gap-2" key={dev.name}>
+                <Avatar size="sm">
+                  <AvatarFallback className="bg-primary">
+                    <User className="text-white" />
+                  </AvatarFallback>
+                  <AvatarImage src={dev.avatar} alt={dev.name} />
+                </Avatar>
+                {dev.github
+                  ? (
+                    <a href={dev.github} target="_blank" rel="noopener noreferrer"
+                      className="text-sm text-muted-foreground hover:text-primary transition-colors">
+                      {dev.name}
+                    </a>
+                  ) : (
+                    <span className="text-sm text-muted-foreground">{dev.name}</span>
+                  )
+                }
+
+              </div>
+            ))}
+          </div>
         </div>
       </section>
 

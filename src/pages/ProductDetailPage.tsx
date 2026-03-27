dadcaed31ebe0c1b89c9239efa72492a0c7694f1
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2026-03-27 20:25:56 +0700

    fix: delete redundant useQuery

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,19 +1,14 @@
 import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
 import ProductDetail from "@/components/product/ProductDetail";
-import { getProductById } from "@/api/product.api";
 import ProductDetailSkeleton from "@/components/skeletons/product/ProductDetailSkeleton";
+import { useProductById } from "@/hooks/queries/useProducts";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
 
-  const { data: product, isLoading, isError } = useQuery({
-    queryKey: ["product", id],
-    queryFn: () => getProductById(Number(id)),
-    enabled: !!id,
-  });
+  const { data: product, isLoading, isError } = useProductById(Number(id));
 
-  if (isLoading) return <ProductDetailSkeleton/>;
+  if (isLoading) return <ProductDetailSkeleton />;
   if (isError || !product) return <div>Product not found</div>;
 
   return <ProductDetail product={product} />;

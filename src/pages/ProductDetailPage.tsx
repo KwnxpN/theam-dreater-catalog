e4d10f948a39f8dd9eb1d400139917ac0d158472
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2026-03-27 12:33:00 +0700

    fix: use tanstack instead

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,33 +1,19 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import ProductDetail from "@/components/product/ProductDetail";
 import { getProductById } from "@/api/product.api";
-import type { Product } from "@/types/product.type";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
-  const [product, setProduct] = useState<Product | null>(null);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      if (!id) return;
+  const { data: product, isLoading, isError } = useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getProductById(Number(id)),
+    enabled: !!id,
+  });
 
-      try {
-        const data = await getProductById(Number(id));
-        setProduct(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProduct();
-  }, [id]);
-
-  if (loading) return <div>Loading...</div>;
-  if (!product) return <div>Product not found</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (isError || !product) return <div>Product not found</div>;
 
   return <ProductDetail product={product} />;
 }

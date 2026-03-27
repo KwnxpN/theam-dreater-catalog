0afd171ab43a54bd8d85993225a66c28b7bcd81a
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2026-03-27 13:31:27 +0700

    feat: add skeleton to product detail

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import ProductDetail from "@/components/product/ProductDetail";
 import { getProductById } from "@/api/product.api";
+import ProductDetailSkeleton from "@/components/skeletons/product/ProductDetailSkeleton";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
@@ -12,7 +13,7 @@ export default function ProductDetailPage() {
     enabled: !!id,
   });
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <ProductDetailSkeleton/>;
   if (isError || !product) return <div>Product not found</div>;
 
   return <ProductDetail product={product} />;

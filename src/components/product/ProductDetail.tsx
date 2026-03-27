4c2b9066b2ba3c09fd1ffc3e8b7a79f5aac2fe7b
KwnxpN <tonnum.kawin@gmail.com>
2026-03-28 00:01:14 +0700

    feat: add back to catalog button to product detail page

--- src/components/product/ProductDetail.tsx
+++ src/components/product/ProductDetail.tsx
@@ -3,6 +3,8 @@ import ProductImages from "./ProductImages";
 import ProductInfo from "./ProductInfo";
 import ReviewSection from "./ReviewSection";
 import { useReviewStats } from "@/hooks/useReviewStats";
+import { ChevronLeft } from "lucide-react";
+import { NavLink } from "react-router-dom";
 type Props = {
   product: Product;
 };
@@ -13,6 +15,14 @@ export default function ProductDetail({ product }: Props) {
   return (
     <div className="min-h-screen text-gray-900 dark:text-white py-10">
       <div className="max-w-6xl mx-auto px-3 md:px-6">
+        <NavLink to="/catalog">
+          <button
+            className="flex items-center gap-2 text-sm text-gray-600 dark:text-gray-400 mb-6"
+          >
+            <ChevronLeft className="size-4" />
+            Back to Catalog
+          </button>
+        </NavLink>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 rounded-xl">
           <ProductImages
             thumbnail={product.thumbnail}
